ec64a9cb8d12430f6b60983a6e22af3d16c8f755
Elias Ylänen <elias.ylanen@gmail.com>
2016-10-05 23:13:26 +0300

    Started API routing

--- server.js
+++ server.js
@@ -28,4 +28,41 @@ app.get('/', (req, res) => {
   res.end(`Hello! This API is at http://localhost:${port}/api`);
 });
 
+app.get('/setup', (req, res) => {
+  const elias = new User({
+    name: 'Elias Ylänen',
+    password: 'password',
+    admin: true,
+  });
+
+  elias.save((err) => {
+    if (err) throw err;
+    console.log('User saved');
+    res.json({ success: true });
+  });
+});
+
+/*
+ * API routes
+ */
+const apiRoutes = express.Router();
+app.use('/api', apiRoutes);
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.get('/authenticate', (req, res) => {
+  res.json(req.body.name);
+});
+// TODO: route middleware to verify a token
+
+apiRoutes.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
+
+apiRoutes.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) throw err;
+    res.json(users);
+  });
+});
+
 app.listen(port);
